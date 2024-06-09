62c164f3dfc6195bbb17535d7a7c7c743ff3cab6
zohebzaman12 <91787135+zohebzaman12@users.noreply.github.com>
2024-06-09 16:48:14 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React, { Suspense, lazy } from 'react'
-import ClickIncrease from './Components/ClickIncrease'
-import HoverIncrease from './Components/HoverIncrease'
-
-const LazyComponent = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('./Components/HoverIncrease')), 5000);
-  });
-});
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="App">
-    {/*Render both of these components to the UI */}
-    <ClickIncrease />
-    <HoverIncrease />
-    <Suspense fallback={<div>Loading...</div>}>
-      <LazyComponent />
-    </Suspense>
-  </div>
+    <div>App</div>
   )
 }
 
